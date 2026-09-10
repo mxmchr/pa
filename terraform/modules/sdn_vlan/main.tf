213d2a94360e27d61eef743f84ccbe307f8aad5e
mxmchr <chretien.maxime72@gmail.com>
2026-09-10 05:08:06 +0200

    docs(decisions): DHCP non disponible sur une zone SDN de type VLAN
    Le DHCP intégré au SDN Proxmox n'est supporté que sur les zones de type
    Simple. La zone PA étant de type VLAN, les dhcp-range déclarées dans
    subnets.cfg sont inopérantes, quelle que soit la configuration.
    
    Le code est conservé en l'état, prêt à être réexploité si le support
    s'étend aux zones VLAN. L'attribution d'adresses est reportée sur
    OPNsense, qui porte déjà une interface sur chaque segment.

--- terraform/modules/sdn_vlan/main.tf
+++ terraform/modules/sdn_vlan/main.tf
@@ -3,6 +3,8 @@ resource "proxmox_sdn_zone_vlan" "this" {
   bridge = var.sdn_bridge
   mtu    = var.mtu
   ipam   = var.ipam
+
+  depends_on = [proxmox_sdn_applier.finalizer]
 }
 
 resource "proxmox_sdn_vnet" "this" {
