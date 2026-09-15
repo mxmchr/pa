2340c8dd613ce7083b16ef8074fda617d06c734b
mxmchr <chretien.maxime72@gmail.com>
2026-09-15 04:26:57 +0200

    feat(identity): remove netbird_client_id output from Terraform configuration

--- terraform/live/04-identity/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "netbird_client_id" {
-  description = "Identifiant client OAuth2, à reporter dans la configuration de NetBird."
-  value       = module.netbird.client_id
-  sensitive   = true
-}
